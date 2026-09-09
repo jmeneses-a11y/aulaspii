9a2443435dd2efdae8ef35d14b6be3e7b032018e
juliana damasceno <j.meneses@escolar.ifrn.edu.br>
2026-09-08 23:20:03 -0300

    Enviando dados do formulário para aplicação

--- src/main/java/ifrn/pi/eventos/models/EventosControllers.java
+++ src/main/java/ifrn/pi/eventos/models/EventosControllers.java
@@ -1,6 +1,8 @@
 package ifrn.pi.eventos.models;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +12,18 @@ public class EventosControllers {
 		return "formEvento";
 	}
 
+	@GetMapping("/evento")
+	public String adicionar1(String nome){
+	    
+	System.out.println("O nome:" +nome);
+		return "index";
+	}
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+ 
+		System.out.println(evento);
+		
+		return "evento-adicionar";
+	
+	}
 }
